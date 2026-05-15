a3341938343f2abb40d83a787c82f8cdd34cf385
Bhavya Reddy <bhavii0230@gmail.com>
2026-05-15 09:32:33 +0530

    fixed production api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 /* API */
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api',
+  baseURL: 'https://eco-ai-backend.onrender.com/api',
 });
 
 /* TYPES */
